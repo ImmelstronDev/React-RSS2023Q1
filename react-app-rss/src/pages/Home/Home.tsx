0ad469e77b91a3c1b4f31b0e1f02b719dd4c60a6
ImmelstronDev <wishandra.afk@gmail.com>
2023-04-16 21:30:58 +0300

    fix: change Home for work with redux

--- react-app-rss/src/pages/Home/Home.tsx
+++ react-app-rss/src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useMemo, useRef, useState } from 'react';
-import { CardData } from 'modules/components/card/card';
-
+import { searchSlice } from '../../store/reducers/SearchSlice';
+import { dataSlice } from '../../store/reducers/DataSlice';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
 import Modal from '../../modules/components/modal/modal';
 import URLs from '../../constants/urlConst';
 import Input from '../../modules/common/input/input';
@@ -8,31 +9,34 @@ import CardList from '../../modules/components/cardList/cardList';
 import cls from './home.module.scss';
 import { HomeContext, ModalState } from './context';
 
-interface CardListState {
-  data: CardData[] | null;
-}
+// interface CardListState {
+//   data: CardData[] | null;
+// }
 
 function Home(): JSX.Element {
-  const key = 'search';
-  const [cardListState, setCardListState] = useState<CardListState>({ data: null });
+  // const key = 'search';
+  // const [cardListState, setCardListState] = useState<CardListState>({ data: null });
   const [isLoading, setLoading] = useState<boolean>(true);
-  const [inputSearchState, setInputSearchState] = useState<string>(localStorage.getItem(key) ?? '');
+  // const [inputSearchState, setInputSearchState] = useState<string>(localStorage.getItem(key) ?? '');
   const [modalData, setModal] = useState<ModalState>({
     isModal: false,
     isLoading: false,
     id: null,
   });
 
-  const refInputSearchValue = useRef<string>(localStorage.getItem(key) ?? '');
+  const dispatch = useAppDispatch();
+  const { cardData } = useAppSelector((state) => state.dataReducer);
+  const { value } = useAppSelector((state) => state.searchReducer);
+  const refInputSearchValue = useRef<string>('');
 
   const { isModal } = modalData;
 
   useEffect(() => {
     const getData = async () => {
       try {
-        const res = await fetch(`${URLs.BASE_URL}?q=${inputSearchState}`);
+        const res = await fetch(`${URLs.BASE_URL}?q=${value}`);
         const prodData = await res.json();
-        setCardListState((old) => ({ ...old, data: prodData }));
+        dispatch(dataSlice.actions.writeDataCard(prodData));
       } catch (error) {
         if (error instanceof Error) {
           throw new Error(error.message);
@@ -42,12 +46,11 @@ function Home(): JSX.Element {
       }
     };
     getData();
-  }, [inputSearchState]);
+  }, [dispatch, value]);
 
   const handleSubmit = () => {
-    if (inputSearchState === refInputSearchValue.current) return;
-    setInputSearchState(refInputSearchValue.current);
-    localStorage.setItem(key, inputSearchState);
+    if (value === refInputSearchValue.current) return;
+    dispatch(searchSlice.actions.writeSearch(refInputSearchValue.current));
     setLoading(true);
   };
 
@@ -78,7 +81,7 @@ function Home(): JSX.Element {
     <HomeContext.Provider value={initialContext}>
       <div className={cls.all_wrapper}>
         <Input refValue={refInputSearchValue} handleSubmit={handleSubmit} />
-        {isLoading ? <div>Loading...</div> : <CardList data={cardListState.data} />}
+        {isLoading ? <div>Loading...</div> : <CardList data={cardData} />}
       </div>
       {isModal && <Modal modalData={modalData} />}
     </HomeContext.Provider>
