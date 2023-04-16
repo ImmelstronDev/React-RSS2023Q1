0167fa138fdabcdbff863e3b1ad4f6b819f1c241
ImmelstronDev <wishandra.afk@gmail.com>
2023-04-16 20:00:32 +0300

    feat: add provider

--- react-app-rss/src/AppRouter.tsx
+++ react-app-rss/src/AppRouter.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { setupStore } from './store/store';
 import App from './App';
 import { HOCProps } from './HOC';
 import Header from './modules/dashboard/components/header/header';
 
 const HeaderWithProps = HOCProps(Header);
+const store = setupStore();
 
 function AppRouter(): JSX.Element {
   return (
     <BrowserRouter>
-      <HeaderWithProps />
-      <App />
+      <Provider store={store}>
+        <HeaderWithProps />
+        <App />
+      </Provider>
     </BrowserRouter>
   );
 }
